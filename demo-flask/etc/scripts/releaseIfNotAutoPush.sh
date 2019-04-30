6706dae0804e823ffeb334b1b7787e87f4bc9815
NerijusVerseckas <nerijusv@gmail.com>
2019-04-30 14:48:43 +0100

    Update releaseIfNotAutoPush.sh

--- demo-flask/etc/scripts/releaseIfNotAutoPush.sh
+++ demo-flask/etc/scripts/releaseIfNotAutoPush.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 #===============================================================
-# IBM Confidential
+# Copyright 2019 IBM Corporation All Rights Reserved.
 #
-# OCO Source Materials
-#
-# Copyright IBM Corp. 2018
-#
-# The source code for this program is not published or otherwise
-# divested of its trade secrets, irrespective of what has been
-# deposited with the U.S. Copyright Office.
+# SPDX-License-Identifier: Apache-2.0
 #===============================================================
 
 #Script that checks if this is an automatied driven release. If so we don't need to trigger another build.
